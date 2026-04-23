7aa0744e6a87f20f420e560b1f9cb96a3af0a04f
victorrosendo <v.rosendo@profesor.duoc.cl>
2026-04-23 15:18:12 -0400

    agregue un comentarios

--- arearectngulo.py
+++ arearectngulo.py
@@ -1,4 +1,4 @@
 largo = int(input("Ingrese el largo del rectángulo: "))
 ancho = int(input("Ingrese el ancho del rectángulo: "))
-
+#linea de codigo
 print(f"El área del rectángulo es: {largo*ancho}")
